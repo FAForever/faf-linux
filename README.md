9185cdd060c6257919f9525fd728e078c581a0b8
iczero <iczero4@gmail.com>
2020-11-23 20:54:14 -0800

    Initial test

---
+++ README.md
@@ -0,0 +1,8 @@
+# FAF on Linux
+
+## How to setup
+
+1. Run Forged Alliance from Steam with Proton at least once (this is necessary to set up proton)
+1. Run `./prepare-prefix.sh` to set up the local prefix, the FAF client, java, and others
+1. Start the FAF client with `./run` and log in
+1. After logging in, close the FAF client and run `./set-client-paths.sh`
---
+++ set-client-paths.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Script to automatically set paths for the FAF client
+
+basedir="$(dirname $(readlink -f "${BASH_SOURCE[0]}"))"
+cd "$basedir"
+
+source ./common-env
+
+client_prefs="$HOME/.faforever/client.prefs"
+
+jq --arg game_path "$game_path" \
+   --arg wrapper "$basedir/launchwrapper "'"%s"' \
+   --arg prefs_path "$game_data_path/Game.prefs" \
+   '.forgedAlliance += { installationPath: ($game_path), executableDecorator: ($wrapper), preferencesFile: ($prefs_path) }' \
+   "$client_prefs" > "$client_prefs".new
+
+mv "$client_prefs".new "$client_prefs"
+echo "Done"
